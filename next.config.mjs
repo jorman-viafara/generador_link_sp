0b2ff6e1ff48aff885f7638595a0de2727c03327
jorman-viafara <jorman.viafara@bluelinkbpo.com.co>
2025-05-20 08:53:50 -0500

    cambios en next.config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  basePath: '/generador-links/superpay',
+  assetPrefix: '/generador-links/superpay',
 }
 
-export default nextConfig
+export default nextConfig;
